8a56846fed938760d44f2eddf6d377ec3cb72e82
QuelleQim <cathrine.karl@gmail.com>
2021-01-20 12:23:10 +0000

    Add paths to urls.py

--- home/views.py
+++ home/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
+from django.urls import path
+from . import views
 
-# Create your views here.
-
-def index(request):
-    """ A view to return the index page """
-    return render(request, 'home/index.html')
+urlpatterns = [
+    path('', views.index, name='home')
+]
