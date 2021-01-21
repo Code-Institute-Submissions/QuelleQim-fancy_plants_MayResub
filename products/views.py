1b62f9878c72a998612d48f0cd9d195505e3dc4a
QuelleQim <cathrine.karl@gmail.com>
2021-01-21 12:35:24 +0000

    Structure product_detail.html and fix bugs so server will run

--- products/views.py
+++ products/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render, get_or_404
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 # Create your views here.
 
 def all_products(request):
-    """ A view to show all products, icluding sorting and search queries """
+    """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
 
@@ -15,17 +15,13 @@ def all_products(request):
     return render(request, 'products/products.html', context)
 
 
-from django.shortcuts import render
-from .models import Product
-
-
 def product_detail(request, product_id):
-    """ A view to show individual products details """
+    """ A view to show individual product details """
 
-    products = get_object_or_404(Product, pk=product_id)
+    product = get_object_or_404(Product, pk=product_id)
 
     context = {
-        'products': products,
+        'product': product,
     }
 
     return render(request, 'products/product_detail.html', context)
