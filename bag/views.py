0262fdb468ee11d426884105abb503bfa20b522d
QuelleQim <cathrine.karl@gmail.com>
2021-02-01 20:51:59 +0000

    Add functionality for adjustation in bag

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 
 # Create your views here.
 
@@ -10,7 +10,7 @@ def view_bag(request):
 
 
 def add_to_bag(request, item_id):
-    """ Add a quantityof the specified product to the shopping bag """
+    """ Add a quantity of the specified product to the shopping bag """
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
@@ -23,3 +23,21 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """ Adjust the quantity of the chosen product to the specified amount """
+
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+
+    # if item_id in list(bag.keys()):
+    #     bag[item_id] += quantity
+    # else:
+    if quantity > 0:
+        bag[item_id] = quantity
+    else:
+        bag.pop[item_id]
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
