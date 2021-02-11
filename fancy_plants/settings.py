0843c6af1f366f520438567ad1f49ba71800a23b
QuelleQim <cathrine.karl@gmail.com>
2021-02-11 13:57:56 +0000

    Add checkout.css, add crispy_forms to installed apps and freeze requirements in pip3

--- fancy_plants/settings.py
+++ fancy_plants/settings.py
@@ -43,6 +43,9 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+
+    #Other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -57,6 +60,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'fancy_plants.urls'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -76,6 +80,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
