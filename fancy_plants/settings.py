3189aa80dd3ccd65abd980458453b26300f65f3a
QuelleQim <cathrine.karl@gmail.com>
2021-02-11 14:13:44 +0000

    Add url path to checkout and change template path

--- fancy_plants/settings.py
+++ fancy_plants/settings.py
@@ -73,11 +73,10 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-
-                # required by allauth
-                'django.template.context_processors.request',
+                'django.template.context_processors.request', # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
             'builtins': [
