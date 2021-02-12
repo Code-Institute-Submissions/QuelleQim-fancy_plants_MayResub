75f97a8b0ec3a027826bc0895b248c0efda0c625
QuelleQim <cathrine.karl@gmail.com>
2021-02-12 13:22:39 +0000

    Add stripe script route in checkout.html

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,7 +1,8 @@
 /*
     Core logic/payment flow for this comes from here:
     https://stripe.com/docs/payments/accept-a-payment
-    CSS from here: 
+
+    CSS comes from here: 
     https://stripe.com/docs/stripe-js
 */
 
