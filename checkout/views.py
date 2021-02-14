699512b8f316544c68d3849d0884f921375b024d
QuelleQim <cathrine.karl@gmail.com>
2021-02-14 12:08:30 +0000

    Change typo in signals.py, add init default app config and basic structure for checkout_success.html

--- checkout/views.py
+++ checkout/views.py
@@ -1,14 +1,13 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.conf import settings
 
 from .forms import OrderForm
-from .models import OrderLineItem
+from .models import Order, OrderLineItem
 from products.models import Product
 from bag.contexts import bag_contents
 
 import stripe
-
 
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
