58f429542b2abf5ce224429b02f45c4a4c52664e
QuelleQim <cathrine.karl@gmail.com>
2021-02-19 13:10:20 +0000

    Add url path and inent to variable

--- checkout/views.py
+++ checkout/views.py
@@ -11,6 +11,7 @@ from bag.contexts import bag_contents
 import stripe
 import json
 
+
 @require_POST
 def cache_checkout_data(request):
     try:
@@ -75,8 +76,7 @@ def checkout(request):
     else:
         bag = request.session.get('bag', {})
         if not bag:
-            messages.error(
-                request, "There's nothing in your bag at the moment")
+            messages.error(request, "There's nothing in your bag at the moment")
             return redirect(reverse('products'))
 
         current_bag = bag_contents(request)
@@ -102,6 +102,7 @@ def checkout(request):
     }
 
     return render(request, template, context)
+
 
 def checkout_success(request, order_number):
     """
