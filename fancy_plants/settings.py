b44632412b892fa027089b34fc96d0816c1c29ed
QuelleQim <cathrine.karl@gmail.com>
2021-02-26 14:49:48 +0000

    Removed secret key and set debug

--- fancy_plants/settings.py
+++ fancy_plants/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-bc%(!ep+(4zutdht6g2cul^3!f19f#c@e(s)-z=4r337fn8+d'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['fancy-plants.herokuapp.com', 'localhost']
 
