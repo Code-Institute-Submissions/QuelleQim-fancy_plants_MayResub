c71f9cb0e3389e76302107296e041f6d1f00e661
QuelleQim <cathrine.karl@gmail.com>
2021-02-26 13:06:45 +0000

    Add conditional for databases in settings.py and freeze requirements

--- fancy_plants/settings.py
+++ fancy_plants/settings.py
@@ -97,12 +97,17 @@ WSGI_APPLICATION = 'fancy_plants.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
