6c9748666044e9063a13a4bddf82c02c19f16e54
QuelleQim <cathrine.karl@gmail.com>
2021-03-03 16:17:25 +0000

    Add views and ulrd for plantcare files

--- plantcare/views.py
+++ plantcare/views.py
@@ -9,3 +9,22 @@ def view_plant_care(request):
 
     return render(request, 'plantcare/plantcare.html')
 
+def water_care(request):
+    """ A view that views the watering plant care page """
+
+    return render(request, 'plantcare/water.html')
+
+def sun_care(request):
+    """ A view that views the sun plant care page """
+
+    return render(request, 'plantcare/sun.html')
+
+def earth_care(request):
+    """ A view that views the earth plant care page """
+
+    return render(request, 'plantcare/earth.html')
+
+def air_care(request):
+    """ A view that views the air plant care page """
+
+    return render(request, 'plantcare/air.html')
