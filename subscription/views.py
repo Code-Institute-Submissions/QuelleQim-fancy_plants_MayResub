b91562b6460924338470a9fd03de234f11ba23ab
QuelleQim <cathrine.karl@gmail.com>
2021-03-03 11:40:01 +0000

    Add views to subscription

--- subscription/views.py
+++ subscription/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.contrib import messages
 
 # Create your views here.
+
+
+def view_subscriptions(request):
+    """ A view that views the subscription page """
+
+    return render(request, 'subscription/subscription.html')
