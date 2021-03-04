04413a6de6b83e0c23fe8c53f67c116a68410953
QuelleQim <cathrine.karl@gmail.com>
2021-03-04 20:24:31 +0000

    Add order to model and subscriptionadmin to admin

--- subscription/admin.py
+++ subscription/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from .models import Subscription
-from profiles.models import UserProfile
 
 
 # Register your models here.
 
 admin.site.register(Subscription)
 
+
+class SubscriptionAdmin(admin.ModelAdmin):
+    readonly_fields = ['get_full_name']
+
+    def get_full_name(self, subscription):
+        return subscription.order.full_name
 
--- subscription/models.py
+++ subscription/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from profiles.models import UserProfile
+from checkout.models import Order
 
 # Create your models here.
 
@@ -11,8 +12,9 @@ class Subscription(models.Model):
     """
     user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
                                      null=True, blank=True, related_name='subscriptions')
+    order = models.ForeignKey(Order, null=False, blank=False,
+                              on_delete=models.CASCADE, related_name='subscriptions')
 
     low_light = models.BooleanField()
     low_humidity = models.BooleanField()
     pet_friendly = models.BooleanField()
-
