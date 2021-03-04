7edfa75e86e415a3114004a6e862f13b8f50b385
QuelleQim <cathrine.karl@gmail.com>
2021-03-04 20:02:33 +0000

    Make migrations, define subscription in admin and add imports and user_profile to models.py

--- subscription/admin.py
+++ subscription/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Subscription
+from profiles.models import UserProfile
+
 
 # Register your models here.
+
+admin.site.register(Subscription)
+
+
--- subscription/models.py
+++ subscription/models.py
@@ -1,12 +1,16 @@
 from django.db import models
+from profiles.models import UserProfile
 
 # Create your models here.
 
-class subscription(models.Model):
+
+class Subscription(models.Model):
     """
     A user profile model for maintaining default
     delivery information and order history
     """
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True, related_name='subscriptions')
 
     low_light = models.BooleanField()
     low_humidity = models.BooleanField()
