a5fb73e27c49e84ce48c95b4ffb2a2bd20d17c07
QuelleQim <cathrine.karl@gmail.com>
2021-03-06 19:54:57 +0000

    Remove field from subscription migration for error search

--- subscription/forms.py
+++ subscription/forms.py
@@ -1,15 +1,17 @@
 from django import forms
+# from .models import UserProfile
+from .models import Subscription
 
 
 class SubscriptionForm(forms.ModelForm):
-    """
-    A user profile model for maintaining default
-    delivery information and order history
-    """
+    class Meta:
+        model = Subscription
 
+    """
+    A user subscription model for maintaining default
+    subscription options
+    """
     low_light = forms.BooleanField()
     low_humidity = forms.BooleanField()
     pet_friendly = forms.BooleanField()
 
-    class Meta:
-        model = subscription
--- subscription/models.py
+++ subscription/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-from profiles.models import UserProfile
-from checkout.models import Order
-from products.models import Product
+# from profiles.models import UserProfile
+# from checkout.models import Order
+# from products.models import Product
 
 # Create your models here.
 
@@ -11,13 +11,15 @@ class Subscription(models.Model):
     A user profile model for maintaining default
     delivery information and order history
     """
-    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
-                                     null=True, blank=True, related_name='subscription')
-    order = models.ForeignKey(Order, null=False, blank=False,
-                              on_delete=models.CASCADE, related_name='subscription')
-    product = models.ForeignKey(
-        Product, null=False, blank=False, on_delete=models.CASCADE)
+    # user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+    #                                  null=True, blank=True, related_name='subscription')
+    # order = models.ForeignKey(Order, null=False, blank=False,
+    #                           on_delete=models.CASCADE, related_name='subscription')
+    # product = models.ForeignKey(
+    #     Product, null=False, blank=False, on_delete=models.CASCADE)
 
     low_light = models.BooleanField()
     low_humidity = models.BooleanField()
     pet_friendly = models.BooleanField()
+
+
